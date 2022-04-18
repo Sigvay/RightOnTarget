d251a076f4704f84590a63cf2e090680cf2f1dc7
Alexander <57791060+Sigvay@users.noreply.github.com>
2022-04-18 22:46:21 +0300

    сделал рефакторинг в соответствии с  MVP

--- RightOnTarget/ViewController.swift
+++ RightOnTarget/ViewController.swift
@@ -8,90 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var number: Int = 0
-    var round: Int = 1
-    var points: Int = 0
+    var game: Game!
     
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
     
     
-    
-    override func loadView() {
-        super.loadView()
-        print("loadView")
-    }
-    
-    
+    // MARK: - Жизненный цикл
     override func viewDidLoad() {
         super.viewDidLoad()
-         print ("viewDidLoad")
-        self.number = Int.random(in: 1...50)
-        self.label.text = String(self.number)
-        // Do any additional setup after loading the view.
+        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("viewWillAppear")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDisappear")
-    }
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewDidDisappear")
-    }
-    
-    
-
-    @IBAction func checkNumber (){
-        let numSlider = Int(self.slider.value.rounded())
-        if numSlider > self.number {
-            self.points += 50 - numSlider + self.number
-        } else if numSlider < self.number {
-            self.points += 50 - self.number + numSlider
+    // MARK: - Взаимодействие View - Model
+    @IBAction func checkNumber(){
+        game.calculateScore(with: Int(slider.value))
+        if game.isGameEnded {
+            showAlertWith(score: game.score)
         } else {
-            self.points += 50
+            game.startNewRound()
         }
-        if self.round == 5 {
-            let alert = UIAlertController(
-                title: "Игра окончена", message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            self.round = 1
-            self.points = 0
-        } else {
-            self.round += 1
-        }
-        self.number = Int.random(in: 1...50)
-        self.label.text = String(self.number)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
     
-    // ленивое свойство для хранения View Controller
-    lazy var secondViewController: SecondViewController =
-    getSecondViewController()
     
-    // приватный метод, загружающий View Controller
-    private func getSecondViewController() -> SecondViewController {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(identifier:
-    "SecondViewController")
-        return viewController as! SecondViewController
+    
+    // MARK: - Обновление View
+    private func updateLabelWithSecretNumber(newText: String){
+        label.text = newText
     }
     
-    @IBAction func showNextScreen() {
-        self.present(secondViewController, animated: true, completion: nil)
+    
+    private func showAlertWith(score: Int) {
+        let alert = UIAlertController(title: "Игра окончена!", message: "Вы заработали \(score) очков", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
     }
-
+    
 }
     
 
